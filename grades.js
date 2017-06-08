a83096c734432451e6313add75a37f01c23e5b97
Aspen Humphrey <aspenjadedimmick@gmail.com>
2017-06-08 15:49:08 -0500

    plugging in some bs

---
+++ grades.js
@@ -0,0 +1,8 @@
+console.log("hello");
+
+var scores = [82, 71, 95, 55, 98, 69, 72, 78, 84, 64, 58, 87];
+
+for (var i = 0; i < scores.length; i++) {
+	//console.log("scores?",scores.length);
+	if (i ){}
+}
